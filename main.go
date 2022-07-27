e59ca2838bfc009258cb4609f29aa4b233b0bfad
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-07-27 09:46:39 +0530

    Print, Println, Printf, Sprint, Sprintf

--- main.go
+++ main.go
@@ -3,32 +3,25 @@ package main // Progams relatd to console based
 import "fmt" // Library which contains functions related to format strings and print them in the console
 
 func main() {
-	// string
-	var name string = "Isuru"
-	var name2 string
-	name3 := "Harischandra"
-	fmt.Println(name, name2, name3)
+	// Print() -> Not adding a new line automatically
+	fmt.Print("Hello \n")
+	fmt.Print("World!")
 
-	// int
-	var num1 int = 234
-	var num2 int8 = 21
-	var num3 int16 = -3863
-	var num4 int32 = 379322
-	var num5 int64
-	num6 := 3343
-	fmt.Println(num1, num2, num3, num4, num5, num6)
+	// Println() -> Adding a new line automatically
+	fmt.Println("Hello")
+	fmt.Println("World!")
 
-	// uint (unsigned int - Doesn't contain negative values)
-	var unum1 uint = 234
-	var unum2 uint8 = 21
-	var unum3 uint16 = 3863
-	var unum4 uint32 = 379322
-	var unum5 uint64
-	fmt.Println(unum1, unum2, unum3, unum4, unum5)
+	// Printf() -> Formatted printing
+	name := "Isuru"
+	age := 23
+	points := 232.23
+	fmt.Printf("My name is %v and my age is %v\n", name, age) // Variables
+	fmt.Printf("My name is %q and my age is %q\n", name, age) // Quotes (Only works for strings)'
+	fmt.Printf("Type of 'age' variable: %T\n", age)           // Type
+	fmt.Printf("Points scored: %f\n", points)                 // Floats
+	fmt.Printf("Points scored: %0.2f\n", points)              // Floats - Limiting decimal points
 
-	// float
-	var fnum1 float32 = 29732.2324
-	var fnum2 float64 = -3023902.23
-	fnum3 := 323.23
-	fmt.Println(fnum1, fnum2, fnum3)
+	// Sprint() -> Save print
+	var str = fmt.Sprintf("My name is %v and my age is %v\n", name, age)
+	fmt.Print("Sentence: ", str)
 }
